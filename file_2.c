e57b9d1ec39313ce13a1f1e638a052bfd4652418
arushi-007 <Arushi.22403@stu.upes.ac.in>
2025-09-30 07:49:19 +0000

    Add file_2.c and update file10.md

---
+++ file_2.c
@@ -0,0 +1,14 @@
+//write a program to take sum of 2 no.as well as product , diff,quotient
+
+#include<stdio.h>
+int main()
+{
+int a=7,b=9,sum,product,diff,quotient;
+sum=a+b;
+diff=a-b;
+product=a*b;
+quotient=a/b;
+printf("%d", sum);
+printf("%d""%d""%d", product, diff, quotient);
+return 0;  
+}
