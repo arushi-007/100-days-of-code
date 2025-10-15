2eb4d4eb00ad9114480c0909a865f6f894ec71d1
arushi-007 <Arushi.22403@stu.upes.ac.in>
2025-10-15 15:42:43 +0000

    code

---
+++ q11-20.md/file19.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+ {
+    int a, b, c;
+    printf("Enter three side lengths: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a == b && b == c)
+        printf("Equilateral triangle\n");
+    else if (a == b || b == c || c == a)
+        printf("Isosceles triangle\n");
+    else
+        printf("Scalene triangle\n");
+    return 0;
+}
